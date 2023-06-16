e4b7c87611411cc3bb0a738c0037c7cd47d1554f
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-06-16 16:10:40 +0200

    title e image

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            
+            title: 'Vue JS',
+            pic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
         }
     }
 });
